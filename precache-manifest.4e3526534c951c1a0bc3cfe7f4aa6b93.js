12c0476862603228977847b1c8571669fffb7455
Wallison Henrique <wallisonhen@gmail.com>
2020-08-25 17:20:27 -0300

    Updates

---
+++ precache-manifest.4e3526534c951c1a0bc3cfe7f4aa6b93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d89271aa343c66f628bc39adc3aa29c",
+    "url": "/aftersale/index.html"
+  },
+  {
+    "revision": "45686aa08b2dcb93a788",
+    "url": "/aftersale/static/js/2.c166fd73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aftersale/static/js/2.c166fd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42b6cc9f487023b2097",
+    "url": "/aftersale/static/js/main.dd7b0f9a.chunk.js"
+  },
+  {
+    "revision": "d15a63458bf5c93f1171",
+    "url": "/aftersale/static/js/runtime-main.b0a5b815.js"
+  }
+]);
